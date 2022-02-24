9d4e997d57d495e6f5cd78fd43d89978a7205d1b
shisan-fei <1340673657@qq.com>
2022-02-24 16:42:13 +0800

    添加普通用户修改密码功能

--- DjangoStudent/apps/DjangoStudent/urls.py
+++ DjangoStudent/apps/DjangoStudent/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^login/', views.login),
     url(r'^show_students/', views.show_students),
-    path('register/', views.register),  # 用于打开注册页面
-    path('register/save', views.save),  # 输入用户名密码后交给后台save函数处理
+    path(r'update/', views.update_pwd),  # 用于打开密码修改页面
+    path(r'update/save', views.save),  # 输入用户名密码后交给后台save函数处理
 ]
